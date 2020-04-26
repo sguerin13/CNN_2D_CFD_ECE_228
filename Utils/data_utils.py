90e97e7e0a625a9a9f7e3ebdbb20fcf2f74a9d32
Steve Guerin <sguerinbb@gmail.com>
2020-04-25 22:38:16 -0700

    Update data_utils.py
    - Changed the shape of the sim data so time is the first axis
    - Added the function that converts the sim data into a pytorch dataloader object

--- Utils/data_utils.py
+++ Utils/data_utils.py
@@ -1,7 +1,12 @@
 import numpy as np
 import torch
-def sim_to_x_y_1_lag(path):
+import torch.utils.data as data
+import torch.utils.data.dataloader as dataloader
+
+
+def sim_to_x_y(path):
     '''
+
     converts the compressed sim data to input and target data with
     y being 1 time step ahead of the x data
 
@@ -16,19 +21,36 @@ def sim_to_x_y_1_lag(path):
     vx = data_file['vx']
     vy = data_file['vy']
     rho = data_file['rho']
+
+    
     # stack along the first dimension
     sim = np.stack((vx,vy,rho),0)
 
+    # swap axes so time is the first axis
+    sim.swapaxes(0,3)
+
     # data gets truncated by 1 due to the lags
-    x_data = np.zeros((sim.shape[0],sim.shape[1],sim.shape[2],sim.shape[3]-1))
-    y_data = np.zeros((sim.shape[0],sim.shape[1],sim.shape[2],sim.shape[3]-1))
+    x_data = np.zeros((sim.shape[0]-1,sim.shape[1],sim.shape[2],sim.shape[3]))
+    y_data = np.zeros((sim.shape[0]-1,sim.shape[1],sim.shape[2],sim.shape[3]))
     
     # iterate over all samples
-    for i in range(sim.shape[-1]-1):
-        x_data[:,:,:,i] = sim[:,:,:,i]
-        y_data[:,:,:,i] = sim[:,:,:,i+1]
+    for i in range(sim.shape[0]-1):
+        x_data[i,:,:,:] = sim[i,:,:,:]
+        y_data[i,:,:,:] = sim[i+1,:,:,:]
 
-    assert x_data[:,:,:,1].all()==y_data[:,:,:,0].all() # sanity check
+    assert x_data[1,:,:,:].all()==y_data[0,:,:,:].all() # sanity check
     return x_data,y_data
 
 
+def np_to_torch_dataloader(x_in,target,batch = 1):
+    '''
+    converts our numpy arrays into a dataloader object by first
+    creating pytorch tensors and then creating a dataloader object
+    from said tensors
+
+    '''
+    x_in_tensor = torch.from_numpy(x_in)
+    target_tensor = torch.from_numpy(target)
+    train_data = data.TensorDataset(x_in_tensor,target_tensor)
+    train_data_loader = dataloader.DataLoader(train_data,batch_size = batch,shuffle = False)
+    return train_data_loader
